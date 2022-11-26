470dbc55996453e985d615870feed8609c5458d1
DmitryScaletta <dmitryscaletta@mail.ru>
2022-11-26 18:20:34 +0300

    ✨ Add index.template.html

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,6 @@
 import fs from 'node:fs/promises';
-import parseDocs from './parseDocs.js';
-import generateTypes from './generateTypes.js';
-import generateApi from './generateApi.js';
-import parseTemplates from './parseTemplates.js';
 import generateOpenApi from './generateOpenApi.js';
+import { HTML_DESCRIPTION, OPEN_API_TITLE } from './utils/constants.js';
 
 const DOCS_URL = 'https://dev.twitch.tv/docs/api/reference';
 const REFERENCE_FILENAME = './tmp/reference.html';
@@ -14,25 +11,22 @@ const fetchDocs = async () => {
 };
 
 const main = async () => {
-  const [html, templatesText, openApiTemplate] = await Promise.all([
+  const [html, openApiTemplate, indexHtmlTemplate] = await Promise.all([
     fetchDocs(),
-    fs.readFile('./scripts/templates.md', 'utf8'),
     fs.readFile('./scripts/openapi.template.json', 'utf8'),
+    fs.readFile('./scripts/index.template.html', 'utf8'),
   ]);
 
-  const apiEndpoints = parseDocs(html);
-  const templates = parseTemplates(templatesText);
-  const types = generateTypes(apiEndpoints);
-  const api = generateApi(apiEndpoints, templates);
   const openapi = generateOpenApi(html, JSON.parse(openApiTemplate));
+  const indexHtml = indexHtmlTemplate
+    .replace('%TITLE%', OPEN_API_TITLE)
+    .replace('%DESCRIPTION%', HTML_DESCRIPTION);
 
   await Promise.all([
     fs.writeFile(REFERENCE_FILENAME, html),
-    fs.writeFile('./api.json', JSON.stringify(apiEndpoints, null, 2)),
-    fs.writeFile('./types.ts', types),
-    fs.writeFile('./api.ts', api),
     fs.writeFile('./openapi.json', JSON.stringify(openapi, null, 2)),
     fs.writeFile('./dist/openapi.json', JSON.stringify(openapi)),
+    fs.writeFile('./dist/index.html', indexHtml),
   ]);
 };
 
