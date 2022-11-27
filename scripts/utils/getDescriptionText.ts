6182a4d58eb998b72bef814f5ffbf4ebfff0ecd3
DmitryScaletta <dmitryscaletta@mail.ru>
2022-11-27 04:06:38 +0300

    🩹 Fix description titles

--- scripts/utils/getDescriptionText.ts
+++ scripts/utils/getDescriptionText.ts
@@ -6,27 +6,27 @@ const getDescriptionText = (descriptions: Descriptions): string => {
   let result = [...descriptions.main];
 
   if (descriptions.authentication.length > 0) {
-    result.push(fmt('Authentication:__'));
+    result.push(fmt('Authentication'));
     result.push(...descriptions.authentication);
   }
 
   if (descriptions.authorization.length > 0) {
-    result.push(fmt('Authorization:__'));
+    result.push(fmt('Authorization'));
     result.push(...descriptions.authorization);
   }
 
   if (descriptions.queryParameters.length > 0) {
-    result.push(fmt('Request Query Parameters:__'));
+    result.push(fmt('Request Query Parameters'));
     result.push(...descriptions.queryParameters);
   }
 
   if (descriptions.requestBody.length > 0) {
-    result.push(fmt('Request Body:__'));
+    result.push(fmt('Request Body'));
     result.push(...descriptions.requestBody);
   }
 
   if (descriptions.responseBody.length > 0) {
-    result.push(fmt('Response Body:__'));
+    result.push(fmt('Response Body'));
     result.push(...descriptions.responseBody);
   }
 
