3b85e75c89c66cb030354ff645bf660032156ee2
DmitryScaletta <dmitryscaletta@mail.ru>
2022-12-17 09:24:37 +0300

    ♻️ Add parseApiReference function

--- scripts/utils/generateOpenApi.ts
+++ scripts/utils/generateOpenApi.ts
@@ -1,7 +1,8 @@
 import { JSDOM } from 'jsdom';
-import type { ApiReference, OpenApi } from '../types';
+import type { OpenApi } from '../types';
 import { OPEN_API_DESCRIPTION, OPEN_API_TITLE } from './constants.js';
 import addUndocumentedApi from './addUndocumentedApi.js';
+import parseApiReference from './parseApiReference.js';
 import parseEndpoint from './parseEndpoint.js';
 
 const generateOpenApi = (
@@ -9,26 +10,8 @@ const generateOpenApi = (
   allScopes: Record<string, string>,
   openApi: OpenApi,
 ): OpenApi => {
-  const apiReference = new Map<string, ApiReference>();
   const { document } = new JSDOM(html).window;
-
-  document
-    .querySelectorAll('#twitch-api-reference + table tbody tr')
-    .forEach((tr) => {
-      const [tagTd, endpointTd, summaryTd] = tr.children;
-      const endpointA = endpointTd!.querySelector('a')!;
-
-      const id = endpointA.getAttribute('href')!.slice(1);
-      const tag = tagTd!.textContent!.trim();
-      const summary = summaryTd!.textContent!.trim();
-
-      apiReference.set(id, {
-        id,
-        tag,
-        summary,
-      });
-    });
-
+  const apiReference = parseApiReference(document);
   const tags = new Set<string>();
   apiReference.forEach(({ tag }) => tags.add(tag));
   openApi.tags = [...tags.values()].map((name) => ({ name }));
---
+++ scripts/utils/parseApiReference.ts
@@ -0,0 +1,24 @@
+import type { ApiReference } from '../types';
+
+const parseApiReference = (document: Document) => {
+  const apiReference = new Map<string, ApiReference>();
+  document
+    .querySelectorAll('#twitch-api-reference + table tbody tr')
+    .forEach((tr) => {
+      const [tagTd, endpointTd, summaryTd] = tr.children;
+      const endpointA = endpointTd!.querySelector('a')!;
+
+      const id = endpointA.getAttribute('href')!.slice(1);
+      const tag = tagTd!.textContent!.trim();
+      const summary = summaryTd!.textContent!.trim();
+
+      apiReference.set(id, {
+        id,
+        tag,
+        summary,
+      });
+    });
+  return apiReference;
+};
+
+export default parseApiReference;
